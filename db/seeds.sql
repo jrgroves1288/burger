a520ccd1dbe93b7af2d1373d4671ccc64d39a2a7
Justin Groves <jrgroves1288@oulook.com>
2020-09-10 21:12:35 -0400

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Buger');
+INSERT INTO burgers (name) VALUES ('BBQ Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name) VALUES ('Bison Buger');
+INSERT INTO burgers (name) VALUES ('Vegatable Burger');
